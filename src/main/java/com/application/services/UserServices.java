76d545848e268b4e405691b934c8a31daa7b66ac
robertkonior <praca.robert.konior@gmail.com>
2019-01-09 23:50:32 +0100

    added logger to services(#7)

--- src/main/java/com/application/services/UserServices.java
+++ src/main/java/com/application/services/UserServices.java
@@ -3,6 +3,8 @@ package com.application.services;
 import com.application.dto.UserDto;
 import com.application.exception.CustomErrorType;
 import com.application.repository.UserJpaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,8 @@ public class UserServices {
 
     @Autowired
     UserJpaRepository repository;
+
+    public static final Logger logger = LoggerFactory.getLogger(UserServices.class);
 
     public ResponseEntity<List<UserDto>> getAllUsers() {
         List<UserDto> users = repository.findAll();
@@ -35,11 +39,13 @@ public class UserServices {
 
     public ResponseEntity<UserDto> addUser(final UserDto user) {
         if (repository.findByName(user.getName()) != null) {
+            logger.error("Unable to create. A User with name {} already exist", user.getName());
             return new ResponseEntity<UserDto>
                     (new CustomErrorType("Unable to create new user. A User with name "
                             + user.getName() + " already exist."), HttpStatus.CONFLICT);
         }
         repository.save(user);
+        logger.info("Creating User : {}", user);
         return new ResponseEntity<UserDto>(user, HttpStatus.CREATED);
     }
 
@@ -48,10 +54,12 @@ public class UserServices {
 
         if (currentUser.isPresent()) {
             repository.saveAndFlush(new UserDto(id, user.getName(), user.getAddress(), user.getEmail()));
+            logger.info("Updating user with id : {}", id);
             return new ResponseEntity<UserDto>(currentUser.get(), HttpStatus.OK);
         }
+        logger.error("Unable to upate. User with id " + id + " not found.");
         return new ResponseEntity<UserDto>(
-                new CustomErrorType("Unable to upate. User with id " + id + " not found."),
+                new CustomErrorType("Unable to update. User with id " + id + " not found."),
                 HttpStatus.NOT_FOUND);
     }
 
@@ -60,10 +68,11 @@ public class UserServices {
 
         if (user.isPresent()) {
             repository.deleteById(id);
+            logger.info("Deleted User with id " + id + ".");
             return new ResponseEntity<UserDto>(new CustomErrorType("Deleted User with id " + id + "."),
                     HttpStatus.NO_CONTENT);
         }
-
+        logger.error("Unable to delete. User with id " + id + " not found.");
         return new ResponseEntity<UserDto>
                 (new CustomErrorType("Unable to delete. User with id " + id + " not found."),
                         HttpStatus.NOT_FOUND);
