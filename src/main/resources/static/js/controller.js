714fad2309a41a599195a794bcb92c35578b3ca4
robertkonior <praca.robert.konior@gmail.com>
2019-01-13 20:45:56 +0100

    list users controller(#17)

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -4,11 +4,11 @@ app.controller('registrationUserController', function ($scope, $http, $window, $
         $http({
             method: 'POST',
             url: 'http://localhost:8080/api/users/',
-            data: $scope.user,
+            data: $scope.user
         }).then(function () {
-                $window.alert("User registered successfully");
-                $location.path("list-all-users");
-                $route.reload();
+            $window.alert("User registered successfully");
+            $location.path("list-all-users");
+            $route.reload();
         }, function (errResponse) {
             $scope.errorMessage = errResponse.data.errorMessage;
         });
@@ -18,3 +18,28 @@ app.controller('registrationUserController', function ($scope, $http, $window, $
         $scope.user = null;
     };
 });
+
+app.controller('listUserController', function ($scope, $http, $location, $route) {
+    $http({
+        method: 'GET',
+        url: 'http://localhost:8080/api/users/'
+    }).then(function (response) {
+        $scope.users = response.data;
+    });
+
+    $scope.editUser = function (userId) {
+        $location.path("/update-users/" + userId);
+    };
+
+    $scope.deleteUser = function (userId) {
+        $http({
+            method: 'DELETE',
+            url: 'http://localhost:8080/api/users/' + userId
+        }).then(
+            function () {
+                $location.path("/list-all-users");
+                $route.reload();
+            });
+    }
+});
+
