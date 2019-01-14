02d1e704263e434dd3a43e107497b7617586c728
robertkonior <praca.robert.konior@gmail.com>
2019-01-14 23:04:55 +0100

    security configuration class(#20)

---
+++ src/main/java/com/application/security/SpringSecurityConfigurationInMemory.java
@@ -0,0 +1,38 @@
+package com.application.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfigurationInMemory extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws  Exception {
+        auth.inMemoryAuthentication().withUser("user").
+                password(passwordEncoder().encode("password")).roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").
+                password("password").roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/users/").hasRole("USER")
+                .antMatchers(HttpMethod.POST,"/api/users/").hasRole("USER")
+                .antMatchers(HttpMethod.PUT,"/api/users/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
+                .and().csrf().disable();
+    }
+}
